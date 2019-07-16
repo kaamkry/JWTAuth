ad531ccf8d190fcd9e284129158ca61e8c6b307d
Kamil Krysiak <kamkry99@gmail.com>
2019-07-16 22:22:29 +0200

    Merge branch 'added exception handling for user'
    # Conflicts:
    #	src/main/java/com/kamkry/app/config/JwtAuthenticationFilter.java
    #	src/main/java/com/kamkry/app/controller/UserController.java

--- src/main/java/com/kamkry/app/config/JwtAuthenticationFilter.java
+++ src/main/java/com/kamkry/app/config/JwtAuthenticationFilter.java
@@ -3,8 +3,12 @@ package com.kamkry.app.config;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.kamkry.app.controller.UserErrorResponse;
 import com.kamkry.app.model.AppUser;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -20,9 +24,11 @@ import java.util.Date;
 
 import static com.kamkry.app.config.SecurityConstants.*;
 
+
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    public final AuthenticationManager authenticationManager;
+
+    private final AuthenticationManager authenticationManager;
 
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
@@ -34,17 +40,38 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         AppUser user = null;
         try {
             user = new ObjectMapper().readValue(request.getInputStream(), AppUser.class);
-
-            return authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            user.getUsername(),
-                            user.getPassword(),
-                            user.getAuthorities()
-                    ));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getUserRoles()
+            );
+
+            return authenticationManager.authenticate(token);
+        } catch (InternalAuthenticationServiceException exception) {
+            try {
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                String json = new Gson().toJson(
+                        new UserErrorResponse(
+                                HttpStatus.NOT_FOUND.value(),
+                                "There's not user: "+user.getUsername(),
+                                System.currentTimeMillis()
+                        )
+                );
+                response.getWriter().println(json);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
+        return null;
     }
+
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
--- src/main/java/com/kamkry/app/controller/UserExceptionHandler.java
+++ src/main/java/com/kamkry/app/controller/UserExceptionHandler.java
@@ -1,27 +1,23 @@
 package com.kamkry.app.controller;
 
-import com.kamkry.app.config.JwtAuthenticationFilter;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@ControllerAdvice
+public class UserExceptionHandler{
 
-@ControllerAdvice(assignableTypes = JwtAuthenticationFilter.class)
-public class UserExceptionHandler {
-
-    @ExceptionHandler(value = {UserNotFoundException.class,InternalAuthenticationServiceException.class})
+    @ExceptionHandler
     public @ResponseBody
-    UserErrorResponse usernameNotFound(RuntimeException exception) {
+    UserErrorResponse usernameNotFound(UserNotFoundException exception) {
         return new UserErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 exception.getMessage(),
                 System.currentTimeMillis()
         );
     }
-
-
 
     @ExceptionHandler
     public @ResponseBody
